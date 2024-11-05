2d481b0a5121b83bddc8ab5859ae67d9c5b8c470
igorecus <102541603+igorecus@users.noreply.github.com>
2024-11-05 23:13:03 +0500

    Add files via upload
    Technical task is completed

---
+++ Trofimov_Python.py
@@ -0,0 +1,19 @@
+#Task 1
+number = int(input()) #10
+name = input() #John
+numbers = list(map(int, input().split())) #3 4 6 8 9 12
+
+if number > 7:
+    print("Hello") #Hello
+
+if name == "John":
+    print("Hello, John") #Hello, John
+else:
+    print("There is no such name")
+
+for num in numbers:
+    if num % 3 == 0:
+        print(num) #3, 6, 9, 12
+
+#Task 2
+#We have to replace the penultimate square bracket [((())()(())]] with a round one [((())()(()))]
